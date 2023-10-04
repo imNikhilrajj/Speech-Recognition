75b0b6ef2befebc7d44de4183cb75e790c058c88
Nikhil Raj <nikhilrajj780@gmail.com>
2023-10-04 20:38:09 +0530

    Add files via upload

---
+++ faqscript.js
@@ -0,0 +1,13 @@
+//for FAQs part
+
+const accordians = document.querySelectorAll('.accordian');
+accordians.forEach(accordian => {
+  const icon = accordian.querySelector('.icon');
+  const answer = accordian.querySelector('.answer');
+
+  accordian.addEventListener('click', () => {
+    console.log('clicked');
+    icon.classList.toggle('active');
+    answer.classList.toggle('active');
+  })
+})
